ae41c5c7e1c6f0add0e46c2002ae98cda3139b78
Vansh 🍭 <vanshvijay9784@gmail.com>
2026-09-06 16:59:49 +0530

    find nth root of a number
    Removed problem statement and approach comments from NthRoot function.

--- Binary_Search/BS_On_Answers/Nth_Root_Of_M.cpp
+++ Binary_Search/BS_On_Answers/Nth_Root_Of_M.cpp
@@ -5,60 +5,6 @@ https://www.naukri.com/code360/problems/nth-root-of-m_1062679?leftPanelTabValue=
 solved on code360
 
 
-## Problem Statement
-You are given two positive integers `n` and `m`.
-Return the **integer nth root of m**, i.e. a number `x` such that:
-
-x^n = m
-
-If the nth root of `m` is **not an integer**, return `-1`.
-
----
-
-## Examples
-
-Input:
-n = 3, m = 27  
-Output:
-3  
-Explanation:
-3^3 = 27
-
-Input:
-n = 2, m = 10  
-Output:
--1  
-Explanation:
-No integer x exists such that x^2 = 10
-
----
-
-## Approach (Binary Search)
-
-- Possible answer range lies between `1` and `m`
-- Use **Binary Search** to reduce time complexity
-- For each `mid`, calculate `mid^n` manually
-- If `mid^n == m`, return `mid`
-- If `mid^n > m`, search left
-- If `mid^n < m`, search right
-
-### Important Points
-- `^` operator in C++ is **XOR**, not power
-- Power is calculated using a loop
-- Early break is used to avoid overflow
-
----
-
-## Algorithm
-1. Set `low = 1`, `high = m`
-2. While `low <= high`:
-   - Calculate `mid`
-   - Compute `mid^n`
-   - Compare with `m`
-3. Return result or `-1`
-
--------------- */
-
 
 int NthRoot(int n, int m) {
 
